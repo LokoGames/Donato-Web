0c25e9c0c2d7caa478e504415614fc61bc634a56
Zeyk <joaovictordonato2@gmail.com>
2022-01-25 18:17:05 -0300

    01/25/2022 18:17:04

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,6 @@
 <?php
     unset($_COOKIE['user']);//, '', time() - 3600, '/');
     header("Set-cookie: user=; expires=1; path=/; HttpOnly; secure=true; SameSite=Strict;");
-    header("Location: ../index.php");
+    header("Location: ../pages/login.html");
     exit;        
 ?>
--- pages/api/login.php
+++ pages/api/login.php
@@ -5,20 +5,24 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
     $res = ($handler->query("SELECT * FROM users;")->fetchAll(PDO::FETCH_ASSOC));
     $username = $_POST["username"];
     $password = $_POST["password"];
-    foreach ($res as $user) { //foreach element in $arr
-        var_dump($user);
-        if ($user["nome"] == $username && $user["password"] == $password && $user["role"] == 0) {
-            $jsonToEncode = array(
-                'username' => $username,
-                'role' => $user["role"]
-            );
-            $json = json_encode($jsonToEncode);
-            header("Set-cookie: user=" . base64_encode($json) . "; expires=0; path=/; HttpOnly; secure=true; SameSite=Strict;");
-            header("Location: ../../admin/");
-            exit;
-        }else {
-            die(header("Location: ../login.html"));
+    var_dump($res);
+    if(count($res) > 0){
+        foreach ($res as $user) { //foreach element in $arr
+            if ($user["nome"] == $username && $user["password"] == $password && $user["role"] == 0) {
+                $jsonToEncode = array(
+                    'username' => $username,
+                    'role' => $user["role"]
+                );
+                $json = json_encode($jsonToEncode);
+                header("Set-cookie: user=" . base64_encode($json) . "; expires=0; path=/; HttpOnly; secure=true; SameSite=Strict;");
+                header("Location: ../../admin/");
+                exit;
+            }else {
+                die(header("Location: ../login.html"));
+            }
         }
+    }else {
+        die(header("Location: ../login.html"));
     }
 } else {
     die(header("Location: ../login.html"));
